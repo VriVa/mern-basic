fd836e5d2b70a3a8864a6baec38191aada8bd3af
Vriddhi Vashi <vvriddhi@gmail.com>
2025-03-17 22:06:07 +0530

    final

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import {userRouter} from './routes/users.js';
 
-import {recipesRouter} from './routes/recipes.js';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
 
-const app =express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/auth', userRouter);
-app.use('/recipes', recipesRouter);
-
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(
-    'mongodb+srv://vriddhirecipe:vriddhirecipepw@recipes.abzle.mongodb.net/recipe?retryWrites=true&w=majority&appName=recipes',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+ 'mongodb+srv://vriddhirecipe:vriddhirecipepw@recipes.abzle.mongodb.net/recipe?retryWrites=true&w=majority&appName=recipes',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-app.listen(3001, () => console.log('Server is running on port 3001'));
+app.listen(3001, () => console.log("Server started"));
--- server/src/models/Recipes.js
+++ server/src/models/Recipes.js
@@ -1,35 +1,34 @@
-import { IconCooker } from "@tabler/icons-react";
 import mongoose from "mongoose";
 
-const RecipeSchema = mongoose.Schema({
+const recipeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  ingredients: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  instructions: {
+    type: String,
+    required: true,
+  },
 
-    name: {
-        type: String,
-        required: true,
-    },      
-    ingredients: 
-        [{type: String,
-        required: true}],
-    
-    instructions: {
-        type: String,
-        required: true,
-    },
-    imageURL: {
-        type: String,
-        required: true,
-    },
-    cookingTime: {
-        type: Number,
-        required: true,
-    },
-    userOwner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        required: true,
-    },
- 
-
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  cookingTime: {
+    type: Number,
+    required: true,
+  },
+  userOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
-export const RecipeModel = mongoose.model("recipes", RecipeSchema);
+export const RecipesModel = mongoose.model("Recipes", recipeSchema);
